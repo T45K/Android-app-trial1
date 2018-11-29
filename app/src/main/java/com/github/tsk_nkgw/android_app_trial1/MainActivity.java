d7ccd6c18007d35e94b8fa14680c702aba71464b
tsk-nkgw <tasktas9@gmail.com>
2018-11-30 00:43:12 +0900

    initial commit

---
+++ app/src/main/java/com/github/tsk_nkgw/android_app_trial1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.tsk_nkgw.android_app_trial1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
